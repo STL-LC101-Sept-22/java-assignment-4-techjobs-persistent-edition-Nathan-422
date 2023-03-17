e504e49f99e6bd3770465b9ef424b189a5be3e44
Nathan-422 <natiouswright@gmail.com>
2023-03-16 21:56:49 -0500

    Added part three and bits of part four

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +15,16 @@ public class Employer extends AbstractEntity {
     @NotNull
     @Size(min=4, max=40)
     private String location;
+
+    // TODO: double check the @JoinColumn annotation has the right name.
+    // https://education.launchcode.org/java-web-development/assignments/tech-jobs-persistent.html#add-a-jobs-field-to-employer
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
     public Employer() {
     }
